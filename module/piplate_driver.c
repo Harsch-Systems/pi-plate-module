3802ecb148e58e436ef3e8f45af8155c2d453fb6
Tyler Stowell <tyler@harschsystems.com>
2020-07-09 17:15:57 +0100

    All plates work with high accuracy now on a loaded pi 0.

--- module/piplate_driver.c
+++ module/piplate_driver.c
@@ -7,12 +7,13 @@
 #include <linux/delay.h>
 #include <linux/sched.h>
 #include <linux/moduleparam.h>
-//linux/completion?
+
 #include "piplate.h"
 
 #define FRAME 25
 #define ACK 23
 
+#define TIME_MAX 1500000
 #define MAX_ID_LEN 25
 
 /*
@@ -80,28 +81,17 @@ static int piplate_release(struct inode *inode, struct file *filp){
 static int piplate_spi_message(struct piplate_dev *dev, struct message *m){
 	struct spi_message tx_msg = { };
 	struct spi_transfer tx_transfer = { };
-	struct spi_message rx_msg = { };
-	struct spi_transfer *rx_transfers;
 	int status;
 	unsigned long j0;
 	ktime_t t0;
 	int attempts = MAX_ATTEMPTS;
-	int rx_len = ((m->bytesToReturn >= 0) ? m->bytesToReturn : MAX_ID_LEN) + m->useACK;
-
-	if(m->bytesToReturn){
-		rx_transfers = kzalloc(rx_len * sizeof(struct spi_transfer), GFP_KERNEL);
-
-		if(!rx_transfers)
-			return -ENOMEM;
-	}
 
 	//To allow it to retry if the transfer fails.
 	start:
-		if(attempts < MAX_ATTEMPTS){
-			if(attempts <= 0){
-				kfree(rx_transfers);
-				return -EIO;
-			}
+		if(attempts <= 0){
+			if(debug_level >= DEBUG_LEVEL_ERR)
+				printk(KERN_ERR "Ran out of attempts due to multiple timing failures\n");
+			return -EIO;
 		}
 
 	dev->tx_buf[0] = m->addr;
@@ -119,9 +109,9 @@ static int piplate_spi_message(struct piplate_dev *dev, struct message *m){
 
 	if(debug_level == DEBUG_LEVEL_ALL){
 		if(m->useACK)
-			printk(KERN_DEBUG "Sending command to Pi Plate using ACK wire (TINKERplate, DAQC2plate, THERMOplate)");
+			printk(KERN_DEBUG "Sending command to Pi Plate using ACK wire (TINKERplate, DAQC2plate, THERMOplate)\n");
 		else
-			printk(KERN_DEBUG "Sending command to Pi Plate without ACK wire (DAQCplate, RELAYplate, MOTORplate)");
+			printk(KERN_DEBUG "Sending command to Pi Plate without ACK wire (DAQCplate, RELAYplate, MOTORplate)\n");
 		printk(KERN_DEBUG "Address: %d\n", m->addr);
 		printk(KERN_DEBUG "Command number: %d\n", m->cmd);
 	}
@@ -145,7 +135,7 @@ static int piplate_spi_message(struct piplate_dev *dev, struct message *m){
 
 	status = spi_sync(dev->spi, &tx_msg);
 
-	if((ktime_get() - t0) > 1500000){//If this process slept for a long time during spi_sync, it needs to restart.
+	if((ktime_get() - t0) > TIME_MAX){//If this process slept for a long time during spi_sync, it needs to restart.
 		gpio_set_value(FRAME, 0);
 		attempts--;
 		udelay(200);//Give piplate time to restart
@@ -160,8 +150,6 @@ static int piplate_spi_message(struct piplate_dev *dev, struct message *m){
 
 	if(m->bytesToReturn){
 		int count = 0;
-		int sum = 0;
-		int verifier = 0;
 
 		//Handles different delay systems. If using ACK, wait for that to go low. Otherwise, delay for 100us.
 		if(m->useACK){
@@ -178,50 +166,125 @@ static int piplate_spi_message(struct piplate_dev *dev, struct message *m){
 			udelay(100);
 		}
 
-		//Creates the receiving transfers. Each byte is a transfer in one message because that allows it to be atomic.
+		if(!m->useACK){
+			//Creates the receiving transfers. Each byte is a transfer in one message because that allows it to be atomic.
+			//m->useACK case is done differently because this method is more accurate, but doesn't work for them.
+			struct spi_message rx_msg = { };
+			struct spi_transfer *rx_transfers;
 
-		spi_message_init(&rx_msg);
+			int rx_len = ((m->bytesToReturn >= 0) ? m->bytesToReturn : MAX_ID_LEN);
 
-		for(count = 0; count < rx_len; count++){
-			rx_transfers[count].len = 1;
-			rx_transfers[count].delay_usecs = 10;
-			rx_transfers[count].rx_buf = &(dev->rx_buf[count]);
-			rx_transfers[count].speed_hz = dev->max_speed_hz;
-			rx_transfers[count].cs_change = 1;
-			spi_message_add_tail(&(rx_transfers[count]), &rx_msg);
-		}
+			rx_transfers = kzalloc(rx_len * sizeof(struct spi_transfer), GFP_KERNEL);
 
-		status = spi_sync(dev->spi, &rx_msg);
+			if(!rx_transfers)
+				return -ENOMEM;
 
-		if(status){
-			if(debug_level >= DEBUG_LEVEL_ERR)
-				printk(KERN_ERR "Unknown error while receiving SPI data\n");
-			goto end;
-		}
+			spi_message_init(&rx_msg);
 
-		for(count = 0; count < (rx_len - m->useACK); count++){
-			unsigned char val = dev->rx_buf[count];
+			for(count = 0; count < rx_len; count++){
+				rx_transfers[count].len = 1;
+				rx_transfers[count].delay_usecs = 10;
+				rx_transfers[count].rx_buf = &(dev->rx_buf[count]);
+				rx_transfers[count].speed_hz = dev->max_speed_hz;
+				rx_transfers[count].cs_change = 1;
+				spi_message_add_tail(&(rx_transfers[count]), &rx_msg);
+			}
 
-			sum += val;
+			status = spi_sync(dev->spi, &rx_msg);
+
+			if(status){
+				if(debug_level >= DEBUG_LEVEL_ERR)
+					printk(KERN_ERR "Unknown error while receiving SPI data\n");
+				kfree(rx_transfers);
+				goto end;
+			}
 
 			if(m->bytesToReturn > 0){
-				m->rBuf[count] = val;
-			}else{
-				if(val){
+				for(count = 0; count < rx_len; count++){
+					unsigned char val = dev->rx_buf[count];
+
 					m->rBuf[count] = val;
-				}else{
-					m->rBuf[count] = '\0';
-					if(m->useACK)
-						verifier = dev->rx_buf[count + 1];
-					count = (rx_len - m->useACK);
+				}
+			}else{
+				for(count = 0; count < rx_len; count++){
+					//Variable Bytes to Return
+					unsigned char val = dev->rx_buf[count];
+
+					if(val){
+						m->rBuf[count] = val;
+					}else{
+						m->rBuf[count] = '\0';
+						count = rx_len;
+					}
 				}
 			}
-		}
 
-		if(m->bytesToReturn > 0 && m->useACK)
-			verifier = dev->rx_buf[rx_len];
+			kfree(rx_transfers);
 
-		if(m->useACK){
+		}else{
+			int sum = 0;
+			int verifier = 0;
+
+			struct spi_message rx_msg = { };
+			struct spi_transfer rx_transfer = { };
+
+			int rx_len = ((m->bytesToReturn >= 0) ? m->bytesToReturn : MAX_ID_LEN) + 1;
+
+			spi_message_init(&rx_msg);
+
+			rx_transfer.len = 1;
+			rx_transfer.delay_usecs = 20;
+			rx_transfer.rx_buf = &dev->rx_buf;
+			rx_transfer.speed_hz = dev->max_speed_hz;
+
+			spi_message_add_tail(&rx_transfer, &rx_msg);
+
+
+			if(m->bytesToReturn > 0){
+				for(count = 0; count < rx_len; count++){
+					status = spi_sync(dev->spi, &rx_msg);
+					if(status){
+						if(debug_level >= DEBUG_LEVEL_ERR)
+							printk(KERN_ERR "Unknown error while receiving SPI data\n");
+						goto end;
+					}
+					m->rBuf[count] = dev->rx_buf[0];
+					if(count < rx_len - 1)
+						sum += dev->rx_buf[0];
+				}
+
+				verifier = dev->rx_buf[0];
+			}else{
+				while(count < rx_len){
+					status = spi_sync(dev->spi, &rx_msg);
+					if(status){
+						if(debug_level >= DEBUG_LEVEL_ERR)
+							printk(KERN_ERR "Unknown error while receiving SPI data\n");
+						goto end;
+					}
+
+					if(dev->rx_buf[0] != 0){
+						m->rBuf[count] = dev->rx_buf[0];
+						sum += dev->rx_buf[0];
+						count++;
+					}else{
+						m->rBuf[count + 1] = '\0';
+
+						status = spi_sync(dev->spi, &rx_msg);
+
+						if(status){
+							if(debug_level >= DEBUG_LEVEL_ERR)
+				 				printk(KERN_ERR "Unknown error while receiving SPI data\n");
+							goto end;
+						}
+
+						verifier = dev->rx_buf[0];
+
+						count = rx_len;
+					}
+				}
+			}
+
 			if((~verifier & 0xFF) != (sum & 0xFF)){
 				if(debug_level >= DEBUG_LEVEL_ERR)
 					printk(KERN_ERR "Error while receiving data: Did not match verification byte\n");
@@ -230,118 +293,8 @@ static int piplate_spi_message(struct piplate_dev *dev, struct message *m){
 			}
 		}
 	}
-/*
-	if(m->bytesToReturn > 0){
-		int i;
-		int max = m->bytesToReturn + m->useACK;//Newer ACK pi plates include extra verification response byte
-		for(i = 0; i < max; i++){
-			status = spi_sync(dev->spi, &msg);
-			if(status){
-				if(debug_level >= DEBUG_LEVEL_ERR)
-					printk(KERN_ERR "Unknown error while receiving SPI data\n");
-				goto end;
-			}
-			m->rBuf[i] = dev->rx_buf[0];
-		}
-
-		if(m->useACK){//Verify that last byte in sum of receiving values equals last received byte inverted
-			int sum = 0;
-			for(i = 0; i < m->bytesToReturn; i++)
-				sum += m->rBuf[i];
-
-		}
-	}else if(m->bytesToReturn == -1){//Just keep receiving bytes until passing 25 or receiving 0. (getID)
-		int count = 0;
-		int sum = 0;
-		struct spi_message message = { };
-
-//
-//
-		status = spi_sync(dev->spi, &message);
-
-		if(status){
-			if(debug_level >= DEBUG_LEVEL_ERR)
-				printk(KERN_ERR "Unknown error during SPI transfer\n");
-			goto end;
-
-		}
-
-		count = 0;
-		while (count < MAX_ID_LEN){
-			unsigned char val = dev->rx_buf[count];
-			if(val != 0){
-				m->rBuf[count] = val;
-				sum += val;
-				count++;
-			}else{
-				m->rBuf[count] = '\0';
-				if(m->useACK){
-					/*
-					//status = spi_sync(dev->spi, &msg);
-					if(status){
-						if(debug_level >= DEBUG_LEVEL_ERR)
-							printk(KERN_ERR "Unknown error while receiving SPI data\n");
-						goto end;
-					}
-
-					if((~(dev->rx_buf[count + 1]) & 0xFF) != (sum & 0xFF)){
-						if(debug_level >= DEBUG_LEVEL_ERR)
-							printk(KERN_ERR "Error while receiving data: Did not match verification byte\n");
-						status = -EIO;
-						goto end;
-					}
-
-				}
-				count = 25;
-			}
-			if(m->useACK){
-				status = spi_sync(dev->spi, &msg);
-			}else{
-				j0 = jiffies;
-				status = spi_sync(dev->spi, &msg);
-				delta = 1000 * (jiffies - j0) / HZ;
-				if(delta > 2)
-					printk(KERN_INFO "Error: %lu\n", delta);
-				stop = ktime_get();
-				delta = (stop - init);
-				init = stop;
-				if(delta > 1500000){//The wait was too long, piplate no longer ready.
-					//printk(KERN_INFO "delay: %lu us\n", delta);
-					goto start;
-				}
-			}
-			if(status)
-				goto end;
-
-			if(dev->rx_buf[count] != 0){
-				m->rBuf[count] = dev->rx_buf[count];
-				sum += dev->rx_buf[count];
-				count++;
-			}else{
-				m->rBuf[count + 1] = '\0';
-				if(m->useACK){
-					//status = spi_sync(dev->spi, &msg);
-					if(status){
-						if(debug_level >= DEBUG_LEVEL_ERR)
-							printk(KERN_ERR "Unknown error while receiving SPI data\n");
-						goto end;
-					}
-					if((~(dev->rx_buf[count + 1]) & 0xFF) != (sum & 0xFF)){
-						if(debug_level >= DEBUG_LEVEL_ERR)
-							printk(KERN_ERR "Error while receiving data: Did not match verification byte\n");
-						status = -EIO;
-						goto end;
-					}
-				}
-				count = 25;
-			}
-		}
-	}
-*/
 
 	gpio_set_value(FRAME, 0);
-
-	kfree(rx_transfers);
 
 	if(debug_level == DEBUG_LEVEL_ALL)
 		printk(KERN_DEBUG "Message sent successfully\n");
@@ -350,7 +303,6 @@ static int piplate_spi_message(struct piplate_dev *dev, struct message *m){
 
 	//Goto statement that ensures the FRAME is set low if an error occurs at any point
 	end:
-		kfree(rx_transfers);
 		gpio_set_value(FRAME, 0);
 		return status;
 }
--- tests/test2.c
+++ tests/test2.c
@@ -13,20 +13,38 @@ int main() {
 		exit(1);
 	}
 
+	int successes = 0;
+
+	unsigned char address = 36;
+	bool ack = 1;
+
 	struct message m = BASE_MESSAGE;
-	m.addr = 43;//24 = relay, 43 = thermo
+	m.addr = address;//24 = relay, 9 = DAQC, 19 = MOTOR, 43 = thermo, 36 = DAQC2, 53 = TINKER
 	m.cmd = 0x01;
 	m.bytesToReturn = -1;
-	m.useACK = 1;
+	m.useACK = ack;
+
+	struct message m2 = BASE_MESSAGE;
+	m2.addr = address;
+	m2.cmd = 0x00;
+	m2.bytesToReturn = 1;
+	m2.useACK = ack;
 
 	while(1){
 		printf("Return: %d\n", ioctl(fileno(fp), PIPLATE_SENDCMD, &m));
 		printf("Result: %s\n", m.rBuf);
-		if(strcmp("Pi-Plate THERMOplate", m.rBuf))//Pi-Plate RELAY, Pi-Plate THERMOplate
+		if(strcmp("Pi-Plate DAQC2plate", m.rBuf))//Pi-Plate RELAY, Pi-Plate THERMOplate, Pi-Plate DAQC2plate
 			break;
+
+		printf("Return: %d\n", ioctl(fileno(fp), PIPLATE_SENDCMD, &m2));
+		printf("Result: %d\n", m2.rBuf[0]);
+		if(m2.rBuf[0] != address)
+			break;
+
+		successes++;
 	}
 
-	printf("Error");
+	printf("Error, made it through %d successes\n", successes);
 
 	fclose(fp);
 
